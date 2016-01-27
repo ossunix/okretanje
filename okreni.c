8af001cadb0364659c4e437ca263fd6198e10fa4
Nikola <nikola@husar.hr>
2016-01-27 14:36:17 +0100

    Code formatting

--- okreni.c
+++ okreni.c
@@ -3,13 +3,16 @@
 
 int okreni(int n, int r)
 {
-    if(n==0)
-        return r;
-    return   okreni(n / 10, r * 10 + n % 10);
+	if(n==0)
+		return r;
+	
+	return   okreni(n / 10, r * 10 + n % 10);
 }
 
 int main()
 {
 	int broj = 123456;
-    printf("%d", okreni(broj, 0));
+	printf("%d", okreni(broj, 0));
+	
+	return 0;
 }
